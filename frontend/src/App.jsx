9fd01835881748c56fcad9a7a58b23eef0e71be1
Amanthakur2002 <125248018+Amanthakur2002@users.noreply.github.com>
2024-04-17 16:32:00 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Dashboard from "./Components/Dashboard";
-axios.defaults.withCredentials = true;
 
 export default function App() {
   return (    
